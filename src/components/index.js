93f31abe42f2dae4a762096bf765902448e5d006
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-08 11:45:34 -0300

    :sparkles:  mock filter added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Filter from './Filter';
 import InfoCard from './InfoCard';
 import Footer from './Footer';
 
 export {
   Header,
+  Filter,
   InfoCard,
   Footer,
 }
