211a42a086018e1f11eaaa6416fce23e56e52e97
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-10 10:25:12 -0300

    :bug: fix constants api

--- src/providers/constants.js
+++ src/providers/constants.js
@@ -1,3 +1,1 @@
-export const BASE_URL = 'http://5e148887bce1d10014baea80.mockapi.io/keycash/challenge';
-// export const PROPERTY = 'property';
-// export const PROPERTY_ID = ':propertyId';
+export const BASE_URL = 'https://5e148887bce1d10014baea80.mockapi.io/keycash/challenge';
