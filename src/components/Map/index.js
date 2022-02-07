7c512b057c69883fbd96187e40b83ce1a6da1d15
julianapedroso <juliana.cristina@hotmail.com.br>
2022-02-07 15:39:15 -0300

    :lock: environment variables created

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,9 +4,11 @@ import './styles.scss';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const Map = ({ lat, lng }) => {
+  const apiKey = `${process.env.REACT_APP_GOOGLE_MAPS_KEY}`;
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyBeYVf7LF5RTEuM5XmpnaEfUFi3xlrgNcI',
+    googleMapsApiKey: apiKey,
   });
 
   return (
@@ -14,10 +16,10 @@ const Map = ({ lat, lng }) => {
       <h1>Localização</h1>
       {isLoaded ? (
         <GoogleMap
-          mapContainerStyle={{width: '100%', height: '100%'}}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
           center={{
             lat: lat,
-            lng: lng
+            lng: lng,
           }}
           zoom={15}
         >
