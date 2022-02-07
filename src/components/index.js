277380283a2b3f27c89ddec86624ee1aa1247901
julianapedroso <juliana.cristina@hotmail.com.br>
2022-02-07 14:44:58 -0300

    :sparkles: map added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import InfoCard from './InfoCard';
+import Map from './Map';
 import Footer from './Footer';
 
 export {
   Header,
   InfoCard,
+  Map,
   Footer,
 }
